fbb8a493ef8d4617dc115017a88ff944efd58628
hamedkavir <h.alikhani@gmail.com>
2018-06-26 13:33:29 +0430

    submit/expense and sumbit/income

--- web/models.py
+++ web/models.py
@@ -3,6 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+
+class Token(models.Model):
+    """User Token"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __unicode__(self):
+        return "{}-token".format(self.user)
+
 # Create your models here.
 class Expense(models.Model):
     text = models.CharField(max_length=255)
--- web/views.py
+++ web/views.py
@@ -2,5 +2,49 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from json import JSONEncoder
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from web.models import User, Token, Expense, Income
+from datetime import datetime
 # Create your views here.
+
+@csrf_exempt
+def submit_expense(request):
+    """ submit an expense """
+
+    #TODO: validate date, user might be fake, token might be fake, amount might be ...
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token= this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    if 'date' in request.POST:
+        date = request.POST['date']
+    Expense.objects.create(user= this_user, amount= request.POST['amount'],
+            text= request.POST['text'], date=date)
+
+    return JsonResponse({
+        'status': 'ok'
+
+    }, encoder=JSONEncoder)
+
+
+
+@csrf_exempt
+def submit_income(request):
+    """ submit an expense """
+
+    #TODO: validate date, user might be fake, token might be fake, amount might be ...
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token= this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    if 'date' in request.POST:
+        date = request.POST['date']
+    Income.objects.create(user= this_user, amount= request.POST['amount'],
+            text= request.POST['text'], date=date)
+
+    return JsonResponse({
+        'status': 'ok'
+
+    }, encoder=JSONEncoder)
